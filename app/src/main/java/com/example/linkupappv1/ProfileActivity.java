63f695f0201a85e399065129ce80dca611b8c10a
64211 <matangawebdev@gmail.com>
2023-10-25 17:33:29 +1300

    WORKING!!!, added bottom navigation bar and changed layout to coordinatelayout for the nav bar to stick to the bottom

--- app/src/main/java/com/example/linkupappv1/ProfileActivity.java
+++ app/src/main/java/com/example/linkupappv1/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.linkupappv1;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,11 +8,14 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -35,6 +39,13 @@ public class ProfileActivity extends AppCompatActivity {
     //Create view objects
     TextView pRequestsCountTV, pLinkUpsCountTV, pRequestsTV, pLinkUpsTV,
             pProfUsername, pProfBio, pProfLocation, pProfInterests;
+    //For menu ids
+    final int homePage = R.id.homePage;
+    final int linkUpPage = R.id.linkupPage;
+    final int profilePage = R.id.profilePage;
+    final int settingsPage = R.id.settingsPage;
+
+    BottomNavigationView bottomNavigationView;
     ImageView pProfilePicIV;
     Uri pProfPicURI;
 
@@ -102,6 +113,10 @@ public class ProfileActivity extends AppCompatActivity {
         pProfUsername = findViewById(R.id.profUsername);
         pProfilePicIV = findViewById(R.id.profileImageView);
         pProfInterests = findViewById(R.id.profInterests);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        //Get Menu ids to use in the onNavigationItemSelected listener
+
         //Create onSetListeners for the TextViews so when a user clicks they can view their requests
         pLinkUpsTV.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -130,9 +145,31 @@ public class ProfileActivity extends AppCompatActivity {
 
             }
         });
+
+        //Set onNavigationItemSelectedListener for bottom navigation created using Material Library
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                //Using if - else to set different navigation item functionalities
+                if (item.getItemId() == R.id.homePage){
+                    //What happens after someone selects homePage item
+                    return true;
+                } else if (item.getItemId() == R.id.linkupPage){
+                    //What happens after someone selects linkUpPage item
+                    return true;
+                } else if (item.getItemId() == R.id.profilePage){
+                    //What happens after someone selects profilePage item
+                    return true;
+                } else if (item.getItemId() == R.id.settingsPage){
+                    //What happens after someone selects settingsPage item
+                    return true;
+                }
+                return false;
+            }
+        });
     }
-
-
 }
 
 
